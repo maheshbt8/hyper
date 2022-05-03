3d80d657e3ad312a41d693ef01f9bf2044261ad6
maheshbt8 <maheshbt8@gmail.com>
2022-05-03 18:52:21 +0530

    api added categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
         if($res->status == '200 OK'){
             $this->branch_id=$res->data->branch[0]->branch_id;
         }else{
-            $this->branch_id=0;
+            $this->branch_id=1;
         }
         //$client = new \Guzzle\Service\Client($this->api_url);
     }
@@ -34,17 +34,18 @@ class HomeController extends Controller
         //echo $this->api_url.'/api/retailer/profile/nearby_store_list/'.$this->store_id;die;
         //$response = Http::get($this->api_url.'/retailer/profile/nearby_store_list/'.$this->store_id.'?customer_latitude=17.4010900&customer_longitude=78.5638922');
         //$response = Http::get($this->api_url.'profile/nearby_store_list/'.$this->store_id.'?customer_latitude=17.4010900&customer_longitude=78.5638922');
+        $response = Http::get($this->api_url.'storefront/rootcategory/list/'.$this->store_id.'/'.$this->branch_id);
         //print_r($response->body());die;
         $res=json_decode($response->body());
         //$res=$res->response;
-        echo "<pre/>";
-        print_r($res);die;
+        //echo "<pre/>";
+        //print_r($res);die;
         if($res->status == '200 OK'){
-            $storedata=['status'=>1,'data'=>$res->data];
+            $categoriesdata=['status'=>1,'data'=>$res->data];
         }else{
-            $storedata=['status'=>0,'data'=>[]];
+            $categoriesdata=['status'=>0,'data'=>[]];
         }
         //return view('layouts/app');
-        return view('website/home',compact('storedata'));
+        return view('website/home',compact('categoriesdata'));
     }
 }
